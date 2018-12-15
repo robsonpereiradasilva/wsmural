f38b8abed75a30a1288c56e44c76a1084135b048
robsonpereiradasilva <robson.silva@novaandradina.org>
2018-12-15 18:51:15 -0300

    a

--- src/main/java/com/mural/controller/VagaController.java
+++ src/main/java/com/mural/controller/VagaController.java
@@ -10,8 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute; 
 import org.springframework.web.client.RestTemplate;
 
 import com.mural.model.Lista;
@@ -63,7 +62,7 @@ public class VagaController {
 			BindingResult result) {
 		List<Vaga> lista = this.vagaRepository.findAll();
 		model.addAttribute("lista", lista);
-		return "index";
+		return "lista";
 		
 	}
 			
--- src/main/java/com/mural/model/Vaga.java
+++ src/main/java/com/mural/model/Vaga.java
@@ -1,6 +1,6 @@
 package com.mural.model;
 
-import javax.annotation.Generated;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
