efe111c452bd75a524060b8e2bf0a6208a514664
robsonpereiradasilva <robson.silva@novaandradina.org>
2018-12-15 19:24:49 -0300

    a

--- src/main/java/com/mural/model/Lista.java
+++ src/main/java/com/mural/model/Lista.java
@@ -3,36 +3,7 @@ package com.mural.model;
 
 public class Lista {
 	
-	private Long id;
-	private String empresa;
 	private String vaga;
-	private String descricao;
-	private float salario;
-	
-	
-	public Lista() {
-		super();
-	}
-
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getEmpresa() {
-		return empresa;
-	}
-
-
-	public void setEmpresa(String empresa) {
-		this.empresa = empresa;
-	}
 
 
 	public String getVaga() {
@@ -43,28 +14,5 @@ public class Lista {
 	public void setVaga(String vaga) {
 		this.vaga = vaga;
 	}
-
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-
-	public float getSalario() {
-		return salario;
-	}
-
-
-	public void setSalario(float salario) {
-		this.salario = salario;
-	}
-	
-	
-	
 
 }
